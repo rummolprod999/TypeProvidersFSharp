4060c71b26f4836ec7cb51564b6beb77f78d74e8
rummolprod999 <rummolprod999@gmail.com>
2019-01-23 15:59:27 +0300

    update

--- TypeProviders.fs
+++ TypeProviders.fs
@@ -1,2 +1,23 @@
 module TypeProvidersFSharp.TypeProviders
+open FSharp.Data
+
+let url = "https://fsharp.github.io/FSharp.Data/data/WorldBank.json"
+type jsonPr = JsonProvider<"https://fsharp.github.io/FSharp.Data/data/WorldBank.json">
+
+let js = jsonPr.Load(url)
+let test = js.Array.[0].Indicator.Id
+
+let urlH = "https://fsharp.github.io/FSharp.Data/library/HtmlProvider.html"
+
+type htmlPr = HtmlProvider<"https://fsharp.github.io/FSharp.Data/library/HtmlProvider.html">
+
+let html = htmlPr.Load(urlH)
+
+let test2 = html.Lists.Html.Body().Elements().Head
+
+type Detailed = XmlProvider<"""<author><name full="true">Karl Popper</name></author>""">
+let info = Detailed.Parse("""<author><name full="false">Thomas Kuhn</name></author>""")
+
+printfn "%s (full=%b)" info.Name.Value info.Name.Full
+
 
