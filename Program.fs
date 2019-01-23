ac13870f52d7f7bb40f45e15fbd01293b3bd6e5a
rummolprod999 <rummolprod999@gmail.com>
2019-01-23 15:58:34 +0300

    Initial commit

---
+++ Program.fs
@@ -0,0 +1,8 @@
+﻿
+module TypeProvidersFSharp.Providers
+open System
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
---
+++ Providers.fs
@@ -0,0 +1,2 @@
+module TypeProvidersFSharp.Providers
+
